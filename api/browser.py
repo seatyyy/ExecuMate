ec429c0ace7423ef752ed52ff014852744b944b0
Kirill Kirikov <kk@datrics.ai>
2025-04-05 18:39:38 -0700

    fixed making order

--- api/browser.py
+++ api/browser.py
@@ -15,6 +15,7 @@ class MenuItem(BaseModel):
     price: float
     restaurant_url: str
     image_url: str
+    delivery_time: str
 
 
 class MenuItems(BaseModel):
@@ -61,9 +62,10 @@ async def find_something(query: str):
 async def find_2_lunch_options():
     task = f"""
     0. Start by going to: https://www.doordash.com/home
-    1. Open the first option under 'Try something new' category on the same tab/page
-    2. From the newly opened tab - extract the restaurant name and restaurant url
-    3. From the newly opened page of restaurant - Extract the item name, item price, and item image URL for the top 3 items listed under the ‘Most Ordered’ section. The item URL is critical.   
+    2. Scroll to 'Try something new' section
+    3. Open the first option under 'Try something new' category on the same tab/page
+    4. From the newly opened tab - extract the restaurant name, restaurant url, delivery time 
+    5. From the newly opened page of restaurant - Extract the item name, item price, and item image URL for the top 3 items listed under the ‘Most Ordered’ section. The item URL is critical.   
     """
     controller = Controller(output_model=MenuItems)
     result = await run_browser_agent(task, controller)
@@ -98,13 +100,14 @@ async def order_food(restaurant_url: str, item_name: str) -> str:
 
     task = f"""
 1. Go to {restaurant_url}
-2. Click on {item_name}
-3. Click "Add to cart"
-4. Wait 3 seconds
-5. Click "Cart" button on the top right corner
-6. Click on "Continue" in the cart
-5. If there are upsell modals, click "Skip"
-6. Click "Place order"
+2. Put into search field the name of the item: {item_name}
+3. Click on {item_name} and wait 2 seconds
+4. Click "Add to cart"
+5. Wait 3 seconds
+6. Click "Cart" button on the top right corner
+7. Click on "Continue" in the cart
+8. If there are upsell modals, click "Skip"
+9. Click "Place order"
 """
 
     # Start the background task for ordering
@@ -125,32 +128,35 @@ async def perform_order(task: str):
 
 if __name__ == "__main__":
     # asyncio.run(find_2_lunch_options())
-    items = {
-      "menu_items": [
-        {
-          "item_name": "Chicken Kebab",
-          "restaurant_name": "Hummus Mediterranean Kitchen",
-          "price": 20.25,
-          "restaurant_url": "https://www.doordash.com/store/902?cursor=eyJzdG9yZV9wcmltYXJ5X3ZlcnRpY2FsX2lkcyI6WzEsNCwxMDAzMzIsMTc1LDE3NiwxNzcsMTc5LDE5MywxOTVdfQ==&pickup=false",
-          "image_url": "https://img.cdn4dd.com/cdn-cgi/image/fit=contain,format=auto,width=800,quality=50/https://doordash-static.s3.amazonaws.com/media/photos/240ebad9-dce0-4a06-874b-7126c9694208-retina-large.jpg"
-        },
-        {
-          "item_name": "Chips & Salsa",
-          "restaurant_name": "Hummus Mediterranean Kitchen",
-          "price": 3.5,
-          "restaurant_url": "https://www.doordash.com/store/902?cursor=eyJzdG9yZV9wcmltYXJ5X3ZlcnRpY2FsX2lkcyI6WzEsNCwxMDAzMzIsMTc1LDE3NiwxNzcsMTc5LDE5MywxOTVdfQ==&pickup=false",
-          "image_url": "https://img.cdn4dd.com/p/fit=cover,width=1200,format=auto,quality=50/media/yelp/9b23282a-01f7-4169-bbd2-8a5ee8bdea67.jpg"
-        },
-        {
-          "item_name": "Crepevine Club",
-          "restaurant_name": "Hummus Mediterranean Kitchen",
-          "price": 22.95,
-          "restaurant_url": "https://www.doordash.com/store/34196?cursor=eyJzdG9yZV9wcmltYXJ5X3ZlcnRpY2FsX2lkcyI6WzEsNCwxNzddfQ==&pickup=false",
-          "image_url": "https://img.cdn4dd.com/p/fit=cover,width=1200,format=auto,quality=50/media/photos/137616b9-2018-46ae-8e8a-07d0860926a8-retina-large-jpeg"
-        }
-      ]
-    }
+    # items = {
+    #   "menu_items": [
+    #     {
+    #       "item_name": "Chicken Kebab",
+    #       "restaurant_name": "Hummus Mediterranean Kitchen",
+    #       "price": 20.25,
+    #       "restaurant_url": "https://www.doordash.com/store/902?cursor=eyJzdG9yZV9wcmltYXJ5X3ZlcnRpY2FsX2lkcyI6WzEsNCwxMDAzMzIsMTc1LDE3NiwxNzcsMTc5LDE5MywxOTVdfQ==&pickup=false",
+    #       "image_url": "https://img.cdn4dd.com/cdn-cgi/image/fit=contain,format=auto,width=800,quality=50/https://doordash-static.s3.amazonaws.com/media/photos/240ebad9-dce0-4a06-874b-7126c9694208-retina-large.jpg"
+    #     },
+    #     {
+    #       "item_name": "Chips & Salsa",
+    #       "restaurant_name": "Hummus Mediterranean Kitchen",
+    #       "price": 3.5,
+    #       "restaurant_url": "https://www.doordash.com/store/902?cursor=eyJzdG9yZV9wcmltYXJ5X3ZlcnRpY2FsX2lkcyI6WzEsNCwxMDAzMzIsMTc1LDE3NiwxNzcsMTc5LDE5MywxOTVdfQ==&pickup=false",
+    #       "image_url": "https://img.cdn4dd.com/p/fit=cover,width=1200,format=auto,quality=50/media/yelp/9b23282a-01f7-4169-bbd2-8a5ee8bdea67.jpg"
+    #     },
+    #     {
+    #       "item_name": "Crepevine Club",
+    #       "restaurant_name": "Hummus Mediterranean Kitchen",
+    #       "price": 22.95,
+    #       "restaurant_url": "https://www.doordash.com/store/34196?cursor=eyJzdG9yZV9wcmltYXJ5X3ZlcnRpY2FsX2lkcyI6WzEsNCwxNzddfQ==&pickup=false",
+    #       "image_url": "https://img.cdn4dd.com/p/fit=cover,width=1200,format=auto,quality=50/media/photos/137616b9-2018-46ae-8e8a-07d0860926a8-retina-large-jpeg"
+    #     }
+    #   ]
+    # }
 
     # asyncio.run(order_food(MenuItem(
     #     items["menu_items"][0]["restaurant_url"], items["menu_items"][0]["item_name"]
     # )))
+
+    asyncio.run(order_food(restaurant_url="https://www.doordash.com/store/paris-baguette-san-mateo-27651459/39908751/?cursor=eyJzZWFyY2hfaXRlbV9jYXJvdXNlbF9jdXJzb3IiOnsicXVlcnkiOiJjaGVlc2UgY2FrZSIsIml0ZW1faWRzIjpbMTcyODU3NTUyNjAsMTcyODU3NTUyNjIsMjQ0MDE0OTEyMTVdLCJzZWFyY2hfdGVybSI6ImNoZWVzZWNha2UiLCJ2ZXJ0aWNhbF9pZCI6LTk5OSwidmVydGljYWxfbmFtZSI6ImFsbCJ9LCJzdG9yZV9wcmltYXJ5X3ZlcnRpY2FsX2lkcyI6WzEsNCwxNDQsMTc2LDE3NywxOTMsMTk1LDI4NF19&pickup=false",
+                            item_name="NY Style Cheesecake Slice"))
